ed8b9d35dc3cf11daf2970fb6386e08ddfe87631
nbyrd9 <nianibyrd@gmail.com>
2021-03-26 11:28:52 -0400

    updates User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+has_many :reviews
+has_many :shows, through: :reviews
+has_secure_password
+      
+validates_uniqueness_of :username
+validates_presence_of :username
+
+
 end

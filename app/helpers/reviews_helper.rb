260f13c59821cb9675e756b5ceaef356446afabb
nbyrd9 <nianibyrd@gmail.com>
2021-04-02 07:19:03 -0400

    adds more validations to review model

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,9 +1,7 @@
 module ReviewsHelper
 
     def show_show_name(r, index)
-    
         r.show.name if index
-        
     end
 
     def show_reviews_path(show)
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,7 @@ class Review < ApplicationRecord
     belongs_to :show 
 
     scope(:rating_search, ->(rating) { self.where("rating >= ?", rating) })
+    validates_presence_of :rating, :location, :comment
     # scope :sort_rating, lambda {order("rating desc")}
     # scope :rating, lambda {where("rating >=0").order("updated_at")
 
